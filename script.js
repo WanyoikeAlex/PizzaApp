599d064a01773ff23e70a714fa384b0436070ce7
AlexWanyoike <alex.wanyoike@aiesec.net>
2024-08-14 17:22:30 +0300

    Reset Button, Add Order, Order Summary Added

--- script.js
+++ script.js
@@ -2,16 +2,17 @@
 
 const inputSize = document.querySelector('.input-size');
 const inputCrust = document.querySelector('.input-crust');
-const inputToppings = document.querySelector('.toppings');
 const inputQuantity = document.querySelector('.input-quantity');
-const placeOrder = document.querySelector('#place-order-button');
-const toppings = document.querySelectorAll('.input-topping');
-const orderSummary = document.querySelector('#order-summary-details');
-const inputLocation = document.querySelectorAll('.input-location');
+const inputLocation = document.querySelector('.input-location');
 const inputUser = document.querySelector('.input-name');
+const toppings = document.querySelectorAll('.toppings');
+const orderSummary = document.querySelector('#order-summary');
 
-console.log(inputUser.value);
+const placeOrder = document.querySelector('#place-order-button');
+const addOrder = document.querySelector('#add-order-button');
+const reset = document.querySelector('#reset-button');
 
+let inputToppings = [];
 let price = [];
 let displayTotal;
 
@@ -28,7 +29,7 @@ const toppingPrice = {
 };
 
 const deliveryPrice = {
-  area: 0,
+  pickup: 0,
   center: 210,
   suburbs: 400,
   uptown: 600,
@@ -36,27 +37,98 @@ const deliveryPrice = {
   outskirts: 450,
 };
 
-inputLocation.forEach(location =>
-  location.addEventListener('click', function (e) {
-    price.push(deliveryPrice[`${e.target.value}`]);
-    console.log(price);
-  })
-);
-
+// Get the toppings Added
 toppings.forEach(topping =>
-  topping.addEventListener('click', function (e) {
-    price.push(toppingPrice[`${e.target.value}`]);
+  topping.addEventListener('change', function (e) {
+    if (!inputToppings.includes(e.target.value)) {
+      inputToppings.push(e.target.value);
+    } else if (inputToppings.includes(e.target.value)) {
+      let index = inputToppings.indexOf(e.target.value);
+      inputToppings.splice(index, 1);
+    }
   })
 );
 
+// Function to Order Pizza
 const orderPizza = function (e) {
-  e.preventDefault();
+  // e.preventDefault();
 
-  price.push(Number(inputQuantity.value) * sizePrice[`${inputSize.value}`]);
+  // Remove all the values in the Summary columns
+  orderSummary.innerHTML = '';
+
+  // Add Location Price
+  // if (inputLocation.value === 'pickup' && inputUser.value === '') {
+  //   alert`Please input Customer Name`;
+  //   return '';
+  // }
+
+  // Get the number of pizza and price Add and Cost
+  let outputQuantity =
+    Number(inputQuantity.value) * sizePrice[`${inputSize.value}`];
+  price.push(outputQuantity);
+
+  // Delivery Cost Based on Area
+  let outputDelivery = deliveryPrice[`${inputLocation.value}`];
+  price.push(outputDelivery);
+
+  // Add toppings Prices
+  inputToppings.forEach(
+    topping =>
+      function () {
+        price.push(toppingPrice[topping]);
+      }
+  );
+
+  // Display total Price for all the products
   displayTotal = price.reduce((accum, total) => accum + total, 0);
+  console.log(price);
 
-  orderSummary.innerHTML = `${inputUser.value} order: ${inputQuantity.value} ${inputSize.value} pizza Cost: ${displayTotal}`;
+  // Display Customer Name
+  const displayName =
+    inputUser.value != ''
+      ? `<p><strong>Customer Name:</strong> <span id="summary-name">${inputUser.value}</span></p>`
+      : '';
+  // HTML Display
+  const html = ` 
+      
+        <h2 class="section-title">Order Summary</h2>
+        <!-- Order summary details will be displayed here -->
+        <div class="order-details">
+          <p><strong>Size:</strong> <span id="summary-size">${inputSize.value}</span></p>
+          <p><strong>Crust:</strong> <span id="summary-crust">${inputCrust.value}</span></p>
+          <p><strong>Toppings:</strong> <span id="summary-toppings">${inputToppings}</span></p>
+          <p><strong>Number of Pizzas:</strong> <span id="summary-quantity">${inputQuantity.value}</span></p>
+          <p><strong>Delivery Location:</strong> <span id="summary-location">${inputLocation.value}</span></p>
+          ${displayName}
+          <p><strong>Total:</strong> <span id="summary-delivery">${displayTotal}</span></p>
+        </div>
+      `;
+
+  // Display Summary for the pizza
+  // orderSummary.innerHTML = html;
+  orderSummary.insertAdjacentHTML('afterbegin', html);
+
+  // RemoveEventListener Pizza
+  placeOrder.removeEventListener('click', orderPizza);
 };
 
 placeOrder.addEventListener('click', orderPizza);
-placeOrder.addEventListener('click', orderPizza);
+
+// Add Order Function
+const newOrder = function (e) {
+  e.preventDefault();
+};
+// Add Order
+addOrder.addEventListener('click', newOrder);
+
+// Button to Reset Fields
+reset.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  inputToppings = [];
+  price = [];
+  displayTotal;
+
+  orderSummary.innerHTML = '';
+  placeOrder.addEventListener('click', orderPizza);
+});
